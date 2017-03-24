0ad0e1a60d26a0561e588bcb055e2bf7217b9015
VSepaha <vineetsepaha@gmail.com>
2017-03-24 18:38:07 -0400

    server works

--- webapp/server3.py
+++ webapp/server3.py
@@ -10,7 +10,7 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # universal name
 host = '0.0.0.0';                       
 z = -1
-port = 10000                                           
+port = 9999                                           
 
 # bind to the port
 serversocket.bind((host, port))                                  
@@ -19,27 +19,37 @@ serversocket.bind((host, port))
 serversocket.listen(5)                                           
 
 data = ""
+i = 0
+table = []
 while True: #accpeting loop
     # establish a connection
 	print("Waiting for connection...")
 	clientsocket,addr = serversocket.accept()
 	print("Got a connection from %s" % str(addr))
-	first = datetime.datetime.now()
 	while True: #reading loop
-		now = datetime.datetime.now()
-		if( (now.second - first.second) >= 10):
-			break
 		data += clientsocket.recv(1024)#read once
-		z = data.find("SGD:") #detect starting token
-		if z == -1:
-			print("NO STARTING KEY FOUND")
+
+		if i == 0: #if it is the first read, check for correct starting token
+			z = data.find("SGD:") #detect starting token
+			if z == -1: #if no starting token found, break connection
+				print("NO STARTING KEY FOUND")
+				break
+
+		end = data.find("SGD:END") #if the final end token is detected, break
+		if end != -1:
 			break
-		data +=clientsocket.recv(1024)
-		r = data.find("\r\n\r\n")#detect ending token
-		if r != -1:#if ending token is detected
-			break
-	print("-----------Arduino Data-----------\n%s" % data.decode('ascii'))
-	data = ""
+
+		r = data.find("\r\n")#detect ending line token
+		if r != -1:# if ending line token is detected
+			i = i+1 # increment index
+			table.append(data) # add the data to the table
+			data = "" # empty the data buffer
+
+	#print out the data received from the arduino
+	print("-----------Arduino Data-----------")
+	for index in range(0, i):
+		print(table[index].decode('ascii'))
+	
 
 clientsocket.close();
 
