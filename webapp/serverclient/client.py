1ea424930f4fd4f805fa5e7b12cec8d08502f05a
VSepaha <vineetsepaha@gmail.com>
2017-03-26 14:50:11 -0400

    clean up

--- webapp/serverclient/client.py
+++ webapp/serverclient/client.py
@@ -5,11 +5,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
 # get local machine name
-<<<<<<< HEAD
 host = "192.168.0.104" #vineets machine                           
-=======
-host = "192.168.0.108" #vineets machine                           
->>>>>>> 223529feef9dc98cb8a910168c4b0e744bb4dcb1
 
 port = 10000
 
