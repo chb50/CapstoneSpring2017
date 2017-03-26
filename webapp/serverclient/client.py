223529feef9dc98cb8a910168c4b0e744bb4dcb1
VSepaha <vineetsepaha@gmail.com>
2017-03-26 14:47:39 -0400

    windows server works

--- webapp/serverclient/client.py
+++ webapp/serverclient/client.py
@@ -5,16 +5,19 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
 # get local machine name
-host = "localhost" #vineets machine                           
+host = "192.168.0.108" #vineets machine                           
 
 port = 10000
 
+print("Establishing Connection")
 # connection to hostname on the port.
 s.connect((host, port))                               
 
 # Receive no more than 1024 bytes
+print("Receiving Data")
 tm = s.recv(1024)                                     
 
+print("Closing Connection")
 s.close()
 
 print("The name's %s" % tm)
